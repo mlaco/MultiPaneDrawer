ce742690021db923ca8d88990b663ba84fd110ae
Morgan Laco <morganlaco@gmail.com>
2019-03-16 17:41:36 -0400

    Create panes

--- app/navigation/drawer/drawer.tsx
+++ app/navigation/drawer/drawer.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { ViewStyle, View, Animated, SafeAreaView } from "react-native"
+import { Text, ViewStyle, View, Animated, SafeAreaView } from "react-native"
 import { NavigationInjectedProps } from "react-navigation"
 
 const CONTAINER: ViewStyle = {
@@ -7,13 +7,52 @@ const CONTAINER: ViewStyle = {
   alignContent: "stretch",
 }
 
-interface DrawerState {}
+interface DrawerState {
+  drawerWidth?: number
+}
 
 /**
  * The Navigation Drawer
  */
 export class Drawer extends React.Component<NavigationInjectedProps, DrawerState> {
+  state = {
+    drawerWidth: 0,
+  }
   render() {
-    return <SafeAreaView />
+    const { drawerWidth } = this.state
+    return (
+      <SafeAreaView
+        onLayout={event => {
+          this.setState({
+            drawerWidth: event.nativeEvent.layout.width,
+          })
+        }}
+        style={{
+          flexDirection: "row",
+          height: "100%",
+        }}
+      >
+        {/* Left Pane */}
+        <Animated.View
+          style={{
+            left: -drawerWidth,
+            width: drawerWidth,
+            backgroundColor: "#00c3e3",
+          }}
+        >
+          <Text>Left Pane</Text>
+        </Animated.View>
+        {/* Right Pane */}
+        <Animated.View
+          style={{
+            left: -drawerWidth,
+            width: drawerWidth,
+            backgroundColor: "#ff4554",
+          }}
+        >
+          <Text>Right Pane</Text>
+        </Animated.View>
+      </SafeAreaView>
+    )
   }
 }
